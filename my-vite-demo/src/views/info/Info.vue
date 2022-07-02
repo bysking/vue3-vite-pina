d46b7b2fa26d2bfcdd167b09c016c6cf511be33e
bysking <bysking@qq.com>
2022-07-02 19:39:22 +0800

    update: api

--- my-vite-demo/src/views/info/Info.vue
+++ my-vite-demo/src/views/info/Info.vue
@@ -31,12 +31,14 @@ export default {
     <div @click="test2">
       {{ rc }}
     </div>
+    <div @click="testToRef">测试toRefs: {{ toRefObj }}</div>
   </div>
 </template>
 
 <!-- 组合式api -->
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, toRefs, computed, watch, watchEffect } from "vue";
+import { useRoute, useRouter } from "vue-router";
 let msg = ref({ a: 1 }); // 普通类型 +
 let rc = reactive({ a: 444 }); // 只能对象类型，数组
 
@@ -47,6 +49,48 @@ let testClick = () => {
 let test2 = () => {
   rc.b = 3;
 };
+
+let toRefObj = reactive({ a: 1, b: 2 });
+
+const { a, b } = toRefs(toRefObj);
+
+let testToRef = () => {
+  a.value = 3;
+};
+
+let testComputed = computed(() => {
+  return 111;
+});
+
+let testComputed2 = computed({
+  get() {},
+  set() {},
+});
+
+// 监听多个值
+watch([a, b], (newVal, oldVal) => {}, { immediate: true, deep: true });
+// 监听单个值
+watch(
+  () => toRefObj.a,
+  () => {}
+);
+
+// 执行首次，然后内部依赖的响应式变量变化会自动执行
+watchEffect(() => {
+  let aa = msg.value.b;
+  console.log("111");
+});
+
+let routers = useRouter();
+watch(
+  () => routers.currentRoute,
+  () => {
+    console.log("路由变化");
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
 
 <style lang="less" scoped></style>
