a32903a6d292d1c819e98349a75cca6cc0fbd74a
bysking <bysking@qq.com>
2022-07-02 17:37:15 +0800

    init: add init vite-pina demo

---
+++ my-vite-demo/src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<!--  -->
+
+<template>
+  <div>home</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+
+  watch: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped></style>
