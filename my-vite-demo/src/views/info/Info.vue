0d29459f621e5153ecf35eaa22dc01e96f81d315
bysking <bysking@qq.com>
2022-07-02 19:21:00 +0800

    update: compare format vue2 3

--- my-vite-demo/src/views/info/Info.vue
+++ my-vite-demo/src/views/info/Info.vue
@@ -1,24 +1,51 @@
-<!--  -->
+<!-- 
 
 <template>
-  <div>info page</div>
+  <div>info page {{ msg }}</div>
 </template>
 
+// 选项式api
 <script lang="ts">
 export default {
-  name: "",
   components: {},
-  data() {
-    return {};
+
+  setup() {
+    let msg = "3333";
+
+    return {
+      msg,
+    };
   },
+};
+</script>
 
-  watch: {},
+<style lang="less" scoped></style> -->
 
-  computed: {},
+<!-- 纯vue3 -->
 
-  mounted() {},
+<template>
+  <div>
+    info page {{ msg }}
+    <div @click="testClick">click</div>
+    <br />
+    <div @click="test2">
+      {{ rc }}
+    </div>
+  </div>
+</template>
 
-  methods: {},
+<!-- 组合式api -->
+<script setup>
+import { ref, reactive } from "vue";
+let msg = ref({ a: 1 }); // 普通类型 +
+let rc = reactive({ a: 444 }); // 只能对象类型，数组
+
+let testClick = () => {
+  msg.value.b = Math.random();
+};
+
+let test2 = () => {
+  rc.b = 3;
 };
 </script>
 
